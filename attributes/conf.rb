7e0fccf7e8987a2a5e13e1f0a7430fdd9fde5bf9
Brian Racer <bracer@gmail.com>
2013-07-28 18:41:30 -0600

    whitespace

--- attributes/conf.rb
+++ attributes/conf.rb
@@ -1,6 +1,4 @@
-
 default['postfixadmin']['conf']['encrypt'] = 'md5crypt' # md5crypt, md5, cleartext
 default['postfixadmin']['conf']['domain_path'] = 'YES'
 default['postfixadmin']['conf']['domain_in_mailbox'] = 'NO'
 default['postfixadmin']['conf']['fetchmail'] = 'NO'
-
